9d6355833a38f944ad3e37c229663fe18b8303c3
MatvX_lm73 <MatvX_lm73@student.itstep.org>
2024-02-28 17:18:50 +0200

    Make HW_3

--- src/components/product_list/product-add.jsx
+++ src/components/product_list/product-add.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductAdd = () => {
+const ProductAdd = ({ addProd }) => {
+
+    const [title, setTitle] = useState('');     //State input
+
+    const [titleError, setTitleError] = useState(null); // состояние для ошибки
+
+    const clickHandler = () => {
+
+        if (title.trim().length === 0) {
+            setTitleError('Поле пусто');
+            return;
+        }
+        addProd(title);
+        setTitle('');
+        setTitleError(null);
+    }
+
+
+
+
     return (
         <div>
-            <input type='text' />
-            <button>Add</button>
-        </div> 
+            <input type='text'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                onKeyDown={(e) => {if(e.code === "Enter") clickHandler()}}
+            />
+            <button onClick={clickHandler}>Add</button>
+            {titleError && <div>{titleError}</div>}
+        </div>
     );
 }
 
--- src/components/product_list/product-item.jsx
+++ src/components/product_list/product-item.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 
 const ProductItem = (task) => {
+    const setModalWindow = (task) => {
+
+        task.setModalActive(true)
+        task.setModalChildren(
+            <>
+
+                <span>Name : {task.title}</span><br />
+                <span>Cost : {task.price}</span><br />
+                <span>Description : {task.description}</span><br />
+                <img src={task.img ? task.img : "https://cdn-icons-png.flaticon.com/256/447/447119.png"} style={{ width: "100px" }}></img>
+
+            </>
+        )
+    }
+
+
+
 
     return (
+
+
+
+
+
         <div className="task-item">
-            <span>category: {task.category}</span>
+            {/* <span>category: {task.category}</span>
             <span>{task.title}</span>
             <span>price: {task.price}</span>
-            <img src={task.image} style={{ width: '100px' }} />
-            <button>delete</button>
-            <hr />
+            <img src={task.image} style={{ width: '100px' }} /> */}
+            <input type='checkbox' defaultChecked={task.done} />
+            <span>{task.title}</span>
+            <button onClick={() => task.removeProd(task.id)}>delete</button>
+            <button onClick={() => setModalWindow(task)}>description</button>
         </div>
     );
 };
